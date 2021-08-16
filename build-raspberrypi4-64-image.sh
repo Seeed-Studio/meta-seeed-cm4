975793d12a0d34f4b0736f7284d599125ee75cdd
bigbearishappy <953308023@qq.com>
2021-08-16 08:55:10 +0000

    Revert "update the build script"
    This reverts commit e093e17154daec3138a2baae40632888d0b0bbd0.

--- build-raspberrypi4-64-image.sh
+++ build-raspberrypi4-64-image.sh
@@ -7,16 +7,16 @@ git clone -b master https://github.com/openembedded/openembedded-core.git
 
 source oe-init-build-env # in build dir
 
-# modify local.conf to build raspberrypi4 64-bit system
-sed -i '/^MACHINE/s/= .*$/= "raspberrypi4-64"/g' conf/local.conf
-sed -i '$aRPI_KERNEL_DEVICETREE_OVERLAYS_append = \" overlays/reTerminal.dtbo\"' conf/local.conf
-sed -i '$aPACKAGECONFIG_append_pn-qtbase = \" eglfs \"' conf/local.conf
-
 bitbake-layers add-layer ../meta-raspberrypi
 bitbake-layers add-layer ../meta-seeed-reterminal
 bitbake-layers add-layer ../meta-qt5
 bitbake-layers add-layer ../meta-openembedded/meta-oe
 bitbake-layers add-layer ../meta-openembedded/meta-python
+
+# modify local.conf to build raspberrypi4 64-bit system
+sed -i '/^MACHINE/s/= .*$/= "raspberrypi4-64"/g' conf/local.conf
+sed -i '$aRPI_KERNEL_DEVICETREE_OVERLAYS_append = \" overlays/reTerminal.dtbo\"' conf/local.conf
+sed -i '$aPACKAGECONFIG_append_pn-qtbase = \" eglfs \"' conf/local.conf
 
 # building image
 bitbake rpi-basic-image
