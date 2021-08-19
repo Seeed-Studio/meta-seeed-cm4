bcbcd2fce30952c0fd0c13fd5ab9a9b7fc68cb06
BBear <13687227078@163.com>
2021-08-20 01:29:06 +0800

    modify the build script to build the minimal img

--- build-raspberrypi4-64-image.sh
+++ build-raspberrypi4-64-image.sh
@@ -8,10 +8,10 @@ git clone -b master https://github.com/openembedded/openembedded-core.git
 source oe-init-build-env # in build dir
 
 bitbake-layers add-layer ../meta-raspberrypi
-bitbake-layers add-layer ../meta-seeed-reterminal
-bitbake-layers add-layer ../meta-qt5
-bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-python
+#bitbake-layers add-layer ../meta-seeed-reterminal
+#bitbake-layers add-layer ../meta-qt5
+#bitbake-layers add-layer ../meta-openembedded/meta-oe
+#bitbake-layers add-layer ../meta-openembedded/meta-python
 
 # modify local.conf to build raspberrypi4 64-bit system
 sed -i '/^MACHINE/s/= .*$/= "raspberrypi4-64"/g' conf/local.conf
@@ -19,10 +19,10 @@ sed -i '/^MACHINE/s/= .*$/= "raspberrypi4-64"/g' conf/local.conf
 #echo "DL_DIR ?= \"/home/xiongjian/cache/yocto_cache/share/downloads\"" >> conf/local.conf
 #grep -q "^SSTATE_DIR ?= \"/home/xiongjian/compile/build_cache/sstate-cache\"$" conf/local.conf || \
 #echo "SSTATE_DIR ?= \"/home/xiongjian/compile/build_cache/sstate-cache\"" >> conf/local.conf
-grep -q "^RPI_KERNEL_DEVICETREE_OVERLAYS_append = \" overlays/reTerminal.dtbo\"$" conf/local.conf || \
-echo "RPI_KERNEL_DEVICETREE_OVERLAYS_append = \" overlays/reTerminal.dtbo\"" >> conf/local.conf
-grep -q "^PACKAGECONFIG_append_pn-qtbase = \" eglfs \"$" conf/local.conf || \
-echo "PACKAGECONFIG_append_pn-qtbase = \" eglfs \"" >> conf/local.conf
+#grep -q "^RPI_KERNEL_DEVICETREE_OVERLAYS_append = \" overlays/reTerminal.dtbo\"$" conf/local.conf || \
+#echo "RPI_KERNEL_DEVICETREE_OVERLAYS_append = \" overlays/reTerminal.dtbo\"" >> conf/local.conf
+#grep -q "^PACKAGECONFIG_append_pn-qtbase = \" eglfs \"$" conf/local.conf || \
+#echo "PACKAGECONFIG_append_pn-qtbase = \" eglfs \"" >> conf/local.conf
 
 # building image
 bitbake rpi-test-image
