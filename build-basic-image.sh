8b16e9f95dfef324dbf0b062910e79d5f8515192
BBear <13687227078@163.com>
2022-03-14 15:00:38 +0800

    remove the useless step of compiling firmware
    Because the github action has a 6 hours compiling time limitation for one compile job.
    So, we tried to compile the basic firmware in the first job. And then use the cache in 
    the next github action job to speed up the compiling of Full-featured firmware.

--- build-basic-image.sh
+++ build-basic-image.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-git clone -b dunfell https://github.com/openembedded/meta-openembedded.git
-git clone -b master git://git.yoctoproject.org/meta-raspberrypi
+git clone -b honister https://github.com/openembedded/meta-openembedded.git
+git clone -b honister git://git.yoctoproject.org/meta-raspberrypi
 
-#change the kernel version to 5.10+
-cd meta-raspberrypi/
-cp -r recipes-kernel/linux/ ../ 
-git checkout dunfell
-rm -r recipes-kernel/linux/
-mv -f ../linux/ recipes-kernel/
-echo "PREFERRED_VERSION_linux-raspberrypi ?= \"5.10.%\"" >> conf/machine/raspberrypi4-64.conf
-cd ..
 
 source oe-init-build-env # in build dir
 
